6192e8a4daa783331e4fe27cc0f3439ff8ac6246
sabinbogati1 <sabin.bogati@studentpartner.com>
2018-05-30 08:40:04 +0545

    Upto 53

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../receipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,6 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
+  recipes : Recipe[] = [
+    new Recipe("A Test Recipe ", "This is simple a test","https://scontent.fktm6-1.fna.fbcdn.net/v/t31.0-8/16462915_10154187308866727_867489171527791712_o.jpg?_nc_cat=0&oh=a74530a6d3e81db8d2260c33e0f6e624&oe=5B84AC61")
+  ]
   constructor() { }
 
   ngOnInit() {
